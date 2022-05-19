3203291b1a4f8540f6039b28eb189dde2037cd17
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-19 11:25:48 +0530

    Oneround function Added

--- logic.js
+++ logic.js
@@ -1,7 +1,53 @@
 function computerPlay()
 {
-    const choice = ['rock','paper','scissors']
-    console.log(choice[Math.floor(Math.random()*3)])
+    const choice = ['rock','paper','scissor']
+    return choice[Math.floor(Math.random()*3)]
 }
 
-computerPlay()
+function oneRound(player,computer)//win=1 lose=-1 draw=0
+{
+    if(player=='rock'){
+        if(computer=='paper'){
+            return -1
+        }
+        else if(computer=='scissor')
+        {
+            return 1
+        }
+        else if (computer=='rock') 
+        {
+            return 0
+        }
+    }
+    else if(player=='paper'){
+        if(computer=='paper'){
+            return 0
+        }
+        else if(computer=='scissor')
+        {
+            return -1
+        }
+        else if (computer=='rock') 
+        {
+            return 1
+        }
+    }
+    else if(player=='scissor'){
+        if(computer=='paper'){
+            return 1
+        }
+        else if(computer=='scissor')
+        {
+            return 0
+        }
+        else if (computer=='rock') 
+        {
+            return -1//('You Lose! Rock beats Scissor')
+        }
+    }
+    else
+    {
+        return oneRound(prompt('Enter valid input:'),computer)
+    }
+}
+
