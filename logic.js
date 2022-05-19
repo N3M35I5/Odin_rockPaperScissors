28841a7c370880dab83ed9d7ff6e8518cf0fd906
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-19 11:26:20 +0530

    game function Added

--- logic.js
+++ logic.js
@@ -51,3 +51,39 @@ function oneRound(player,computer)//win=1 lose=-1 draw=0
     }
 }
 
+function game()
+{
+    let playerInput = prompt("Choose Rock, Paper, Scissor").toLowerCase()
+    let computerInput=computerPlay()
+    let playerScore=0
+    let computerScore=0
+    for(let i=0;i<5;i++)
+    {
+        result=oneRound(playerInput,computerInput)
+        if(result==0)
+        {
+            console.log("It's a Draw!")
+        }
+        else if(result==1)
+        {
+            console.log('You Win! '+playerInput+' beats '+computerInput)
+            //console.log('You Win! '+playerInput[0].toUpperCase()+playerInput[1,-1]+' beats '+computerInput)
+            playerScore+=1
+        }
+        else if(result==-1)
+        {
+            console.log('You Lose! '+computerInput+' beats '+playerInput)
+            computerScore+=1
+        }
+    }
+    if (computerScore>playerScore)
+    {
+        console.log('Computer Wins with '+computerScore+' points')
+    }
+    else if (computerScore<playerScore)
+    {
+        console.log('You Win with '+playerScore+' points')
+    }
+}
+
+game()
