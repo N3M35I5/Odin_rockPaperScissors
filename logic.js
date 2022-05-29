50c593617e74de29b11c3a2d6e7bfb99a8071b35
N3M35I5 <adwaith1998kv@gmail.com>
2022-05-29 18:11:06 +0530

    Added scoring and results to UI

--- logic.js
+++ logic.js
@@ -51,45 +51,6 @@ function oneRound(player,computer)//win=1 lose=-1 draw=0
     }
 }
 
-function game()
-{
-    let playerScore=0;
-    let computerScore=0;
-    for(let i=0;i<5;i++)
-    {
-        let playerInput = prompt("Choose Rock, Paper, Scissor").toLowerCase();
-        let computerInput=computerPlay();
-        result=oneRound(playerInput,computerInput);
-        if(result==0)
-        {
-            console.log("It's a Draw!");
-        }
-        else if(result==1)
-        {
-            console.log('You Win! '+playerInput[0].toUpperCase()+playerInput.slice(1)+' beats '+computerInput[0].toUpperCase()+computerInput.slice(1));
-            //console.log('You Win! '+playerInput[0].toUpperCase()+playerInput[1,-1]+' beats '+computerInput)
-            playerScore+=1;
-        }
-        else if(result==-1)
-        {
-            console.log('You Lose! '+computerInput[0].toUpperCase()+computerInput.slice(1)+' beats '+playerInput[0].toUpperCase()+playerInput.slice(1));
-            computerScore+=1;
-        }
-    }
-    if (computerScore>playerScore)
-    {
-        console.log('Computer Wins with '+computerScore+' points');
-    }
-    else if (computerScore<playerScore)
-    {
-        console.log('You Win with '+playerScore+' points');
-    }
-    else
-    {
-        console.log("It's a draw!");
-    }
-}
-
 function game2(playerInput,div)
 {
     let computerInput=computerPlay();
@@ -97,15 +58,130 @@ function game2(playerInput,div)
     if(result==0)
     {
         div.textContent ="It's a Draw!";
+        return result;
     }
     else if(result==1)
     {
         div.textContent ='You Win! '+playerInput[0].toUpperCase()+playerInput.slice(1)+' beats '+computerInput[0].toUpperCase()+computerInput.slice(1);
+        return result;
     }
     else if(result==-1)
     {
         div.textContent ='You Lose! '+computerInput[0].toUpperCase()+computerInput.slice(1)+' beats '+playerInput[0].toUpperCase()+playerInput.slice(1);
+        return result;
     }
+}
+
+function stage()
+{
+    let res;
+    let playerScore = 0;
+    let computerScore = 0;
+    let counter=0;
+    const rock = document.querySelector('#rock');
+    rock.addEventListener('click',() => {
+        //console.log(oneRound('rock',computerPlay()));
+        if(counter<5)
+        {
+            res = game2('rock',div);
+            if(res==-1)
+            {
+                computerScore+=1;
+            }
+            else if(res==1)
+            {
+                playerScore+=1;
+            }
+            counter+=1;
+            playerScoreDiv.textContent = `Player score - ${playerScore}`;
+            computerScoreDiv.textContent = `Computer score - ${computerScore}`;
+        }
+        if(counter>=5)
+        {
+            if (computerScore>playerScore)
+            {
+                winnerDiv.textContent='Computer Wins with '+computerScore+' points';
+            }
+            else if (computerScore<playerScore)
+            {
+                winnerDiv.textContent='You Win with '+playerScore+' points';
+            }
+            else
+            {
+                winnerDiv.textContent="It's a draw!";
+            }
+        }
+    });
+
+    const paper = document.querySelector('#paper');
+    paper.addEventListener('click',() => {
+        //console.log(oneRound('rock',computerPlay()));
+        if(counter<5)
+        {
+            res = game2('paper',div);
+            if(res==-1)
+            {
+                computerScore+=1;
+            }
+            else if(res==1)
+            {
+                playerScore+=1;
+            }
+            counter+=1;
+            playerScoreDiv.textContent = `Player score - ${playerScore}`;
+            computerScoreDiv.textContent = `Computer score - ${computerScore}`;
+        }
+        if(counter>=5)
+        {
+            if (computerScore>playerScore)
+            {
+                winnerDiv.textContent='Computer Wins with '+computerScore+' points';
+            }
+            else if (computerScore<playerScore)
+            {
+                winnerDiv.textContent='You Win with '+playerScore+' points';
+            }
+            else
+            {
+                winnerDiv.textContent="It's a draw!";
+            }
+        }
+    });
+
+    const scissor = document.querySelector('#scissor');
+    scissor.addEventListener('click',() => {
+    //console.log(oneRound('rock',computerPlay()));
+        if(counter<5)
+        {
+            res = game2('paper',div);
+            if(res==-1)
+            {
+                computerScore+=1;
+            }
+            else if(res==1)
+            {
+                playerScore+=1;
+            }
+            counter+=1;
+            playerScoreDiv.textContent = `Player score - ${playerScore}`;
+            computerScoreDiv.textContent = `Computer score - ${computerScore}`;
+        }
+        if(counter>=5)
+        {
+            if (computerScore>playerScore)
+            {
+                winnerDiv.textContent='Computer Wins with '+computerScore+' points';
+            }
+            else if (computerScore<playerScore)
+            {
+                winnerDiv.textContent='You Win with '+playerScore+' points';
+            }
+            else
+            {
+                winnerDiv.textContent="It's a draw!";
+            }
+        }
+    });
 }
 
 /*Steps
@@ -114,25 +190,14 @@ function game2(playerInput,div)
 
 const div = document.createElement('div');
 
+const playerScoreDiv = document.createElement('div');
+const computerScoreDiv = document.createElement('div');
+const winnerDiv = document.createElement('div');
 
-const rock = document.querySelector('#rock');
-rock.addEventListener('click',() => {
-    //console.log(oneRound('rock',computerPlay()));
-    game2('rock',div)
-  });
-
-const paper = document.querySelector('#paper');
-paper.addEventListener('click',() => {
-    //console.log(oneRound('rock',computerPlay()));
-    game2('paper',div)
-});
-
-const scissor = document.querySelector('#scissor');
-scissor.addEventListener('click',() => {
-    //console.log(oneRound('rock',computerPlay()));
-    game2('rock',div)
-    });
+stage();
 
 const container = document.querySelector('#container');
 container.appendChild(div)
-
+container.appendChild(playerScoreDiv)
+container.appendChild(computerScoreDiv)
+container.appendChild(winnerDiv)
